5bc192b0d1f3e954f339b820b76050dd28f6a0e4
theslimreaper <jvann@ltu.edu>
2015-09-06 19:29:47 -0400

    spin to win. Working on dice roller.

---
+++ DnD/Assets/Scripts/Screen_Rotator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screen_Rotator : MonoBehaviour {
+	int currentScreen;
+	public GameObject[] arms;
+	public int transitionSpeed;
+	int moving;
+	// Use this for initialization
+	void Start () {
+	
+		for(int i=0;i<arms.Length;i++)
+		{
+			arms[i].transform.Rotate(Vector3.up*(360/arms.Length*i));
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+		   {
+			moving=1;
+			}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			moving=-1;
+		}
+		if(moving!=0)
+			{
+				transform.Rotate(Vector3.up * transitionSpeed*moving);
+
+				if(transform.rotation.eulerAngles.y%(360/arms.Length)<1)
+				{
+					print ("The current Rotation is:"+transform.rotation);
+					moving=0;
+				}
+			}
+	}
+
+}
