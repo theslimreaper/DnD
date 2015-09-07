5cfad605ad4a5e5291e7333356da8fade76e6252
theslimreaper <jvann@ltu.edu>
2015-09-06 20:20:23 -0400

    moved stuff and set up parsing for string to int for dice info

---
+++ DnD/Assets/Scripts/Dice_Rolling.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Dice_Rolling : MonoBehaviour {
+	int diceAmount=0;
+	int diceSides=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RollDice()
+	{
+		diceAmount =System.Convert.ToInt32(GameObject.Find ("Amount of Dice").GetComponent<InputField> ().text);
+		diceSides=System.Convert.ToInt32(GameObject.Find ("SideofDice").GetComponent<InputField> ().text);
+		print (diceAmount + " " + diceSides);
+	}
+
+}
