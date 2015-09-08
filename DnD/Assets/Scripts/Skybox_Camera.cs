ba6ec07b1902ec9dff94d6b307587eb05b571f3a
unknown <mborsos@MP05VJK7.campus.ltu.edu>
2015-09-07 21:19:31 -0400

    Added "Simple Cloud System" asset to create rotating skybox with cloud background.
    Note that the skybox does not fully work as if the user moves to a new screen the skybox will not change its content (i.e. does not work like a fully dynamic sky). Some scripting should fix this.

---
+++ DnD/Assets/Scripts/Skybox_Camera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skybox_Camera : MonoBehaviour {
+	private Vector3 rotationValue; 
+	private float turnValue = 0.0f; 
+	private float turnVal { get { return turnValue; } set { turnValue = value; if (turnValue >= 360f) turnValue = 0.0f; } }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		turnVal += Time.deltaTime;
+		rotationValue = new Vector3 (Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y + turnVal, Camera.main.transform.rotation.eulerAngles.z);
+		transform.rotation = Quaternion.Euler (rotationValue);
+	}
+
+}
