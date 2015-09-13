579a3599209d6a7eb22f45f99a2e88d067a39bab
unknown <mborsos@MP05VJK7.campus.ltu.edu>
2015-09-13 11:59:49 -0400

    Created XML_Loader script which will handle the loading of XML files.
    Currently I am referencing a w3schools url but this code will need to be optimized to consider all of the XML files we need to load in.

---
+++ DnD/Assets/Scripts/XML_Loader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Net;
+
+
+public class XML_Loader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LoadXml ();
+	}
+
+	public void LoadXml () {
+		var m_strFilePath = "http://www.w3schools.com/xml/note.xml";
+		string xmlStr;
+		using(var wc = new WebClient())
+		{
+			xmlStr = wc.DownloadString(m_strFilePath);
+		}
+		var xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml(xmlStr);	
+		print (xmlStr);
+	}
+
+}
