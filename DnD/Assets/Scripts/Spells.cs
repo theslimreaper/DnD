a900c6b12eaeb65c3e96e9b69ccdc3d3b94f75fa
unknown <mborsos@MP05VJK7.campus.ltu.edu>
2015-09-13 20:32:04 -0400

    Created spells class (Spells are currently on arm 3) Also made the XML_Loader class more dynamic by having 1 public method (LoadXml) to get the list of grouped elements (spells, feats, etc) for the specified url and the specified tag name of the grouped elements (see Spells.cs for example).

---
+++ DnD/Assets/Scripts/Spells.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spells : MonoBehaviour {
+	public List<string> spells = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		XML_Loader XML = ScriptableObject.CreateInstance<XML_Loader> ();
+
+		spells = XML.LoadXml ("https://raw.githubusercontent.com/theslimreaper/DnD/master/XML%20Files/Spells/spells.xml", "spell");
+		
+		foreach (var item in spells) {
+			print (item);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- DnD/Assets/Scripts/XML_Loader.cs
+++ DnD/Assets/Scripts/XML_Loader.cs
@@ -1,34 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.Xml;
 using System.Net;
 
+public class XML_Loader : ScriptableObject {
 
-public class XML_Loader : MonoBehaviour {
-	
-	// Use this for initialization
-	void Start () {
-		LoadXml ();
+	public List<string> LoadXml(string url, string tagName) {
+		XmlNodeList elemList = null;
+		var doc = new XmlDocument();
+		List<string> strList = new List<string>();
+
+		doc = GetXmlContent (url);
+
+		elemList = GetXmlElems (doc, tagName);
+
+		strList = ConvertElemsToList (elemList);
+
+		return strList;
 	}
-
 	//Loads specified XML file content
-	public void LoadXml () {
-		var m_strFilePath = "https://raw.githubusercontent.com/theslimreaper/DnD/master/XML%20Files/Spells/spells.xml";
+	XmlDocument GetXmlContent (string url) {
+		var m_strFilePath = url;
 		string xmlStr;
-		var xmlDoc = new XmlDocument();
-
+		var doc = new XmlDocument();
+		
 		//Get SSL Certificate Validated
 		System.Net.ServicePointManager.ServerCertificateValidationCallback += (s,ce,ca,p) => true;
-
+		
 		//Get file from URL
 		using(var wc = new WebClient())
 		{
 			xmlStr = wc.DownloadString(m_strFilePath);
 		}
-
+		
 		//Load content of file
-		xmlDoc.LoadXml(xmlStr);    
-		print (xmlStr);
+		doc.LoadXml(xmlStr);   
+		
+		return doc;
+	}
+
+	//Get elements of XML file based on groups of a tag name
+	XmlNodeList GetXmlElems (XmlDocument doc, string tagName) {
+		//Organize sections of file (spells, classes, etc.) into lists based on tag
+		XmlNodeList elemList = doc.GetElementsByTagName(tagName);
+
+		return elemList;
+	}
+
+	//Convert the element list into a string list
+	List<string> ConvertElemsToList (XmlNodeList elemList) {
+		List<string> strList = new List<string>();
+
+		for (int i=0; i < elemList.Count; i++)
+		{   
+			strList.Add(elemList[i].OuterXml);
+		}  
+
+		return strList;
 	}
 	
 }
