f2806c555b0fc55bc5064ea0f1b15a7d6bffa57f
theslimreaper <jvann@ltu.edu>
2015-09-17 22:27:30 -0400

    basic start screen scene and script

---
+++ DnD/Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void createNewCharacter()
+	{
+		Application.LoadLevel ("Base");
+	}
+	public void loadExistingCharacter()
+	{
+		print ("feature not available yet");
+	}
+	public void exitGame()
+	{
+		Application.Quit ();
+	}
+	public void aboutUs()
+	{
+		Application.LoadLevel ("Credits");
+	}
+
+}
