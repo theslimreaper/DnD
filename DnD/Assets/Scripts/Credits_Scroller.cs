88ca334fb3b71f5e0f891b7b071adb53d04d0206
Joel Vann <Jvann@ltu.edu>
2015-09-22 23:52:00 -0400

    Working on getting skills sheet working and found problem with the canvas in base scene. Spent about an hour trying to get it working. Please take a look and see if you can get it working, this is halting any testing for the base scenes functionality.

---
+++ DnD/Assets/Scripts/Credits_Scroller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits_Scroller : MonoBehaviour {
+    public GameObject credits;
+    public float speed = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        credits.transform.position= new Vector3 (credits.transform.position.x,credits.transform.position.y+.5f*speed,credits.transform.position.z);
+	}
+}
