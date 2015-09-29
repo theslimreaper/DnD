def3a671a5ff6e9d7381d0427e464643284a38bd
Joel Vann <Jvann@ltu.edu>
2015-09-28 22:13:07 -0400

    merged mine and marshalls version of scene. done with majority of skill page

--- DnD/Assets/Scripts/Skill_Handler.cs
+++ DnD/Assets/Scripts/Skill_Handler.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Skill_Handler : MonoBehaviour {
-
+    public static string[] skillNames = {"Acrobatics","Animal Handling","Arcana","Athletics","Deception","History",
+                                        "Insight","Intimidation","Investigation","Medicine","Nature","Perception",
+                                        "Performance", "Persuasion", "Relgion", "Sleight of Hand", "Stealth", "Survival"};
     public bool[] trained = new bool[18];
-    public int[] values = new int[18];
+    public static int[] values = new int[18];
     public int[] bonuses = new int[18];
 	// Use this for initialization
 	void Start () {
--- DnD/Assets/Scripts/TrainedSkillChecker.cs
+++ DnD/Assets/Scripts/TrainedSkillChecker.cs
@@ -4,10 +4,29 @@ using System.Collections;
 
 public class TrainedSkillChecker : MonoBehaviour {
     public int skillNumber;
-
+    public InputField extraModifier;
+    public Text checkResults;
+    public int lastResult;
+    public GameObject ResultBG;
+    public GameObject SkillResultsText;
     public void onclickRoll()
     {
-       
+        lastResult = 0;
+        lastResult += Skill_Handler.values[skillNumber-1];
+        if(extraModifier.text!=null&& extraModifier.text!="")
+        {
+            lastResult += System.Convert.ToInt32(extraModifier.text);
+        }
+
+        lastResult += Random.Range(1, 21);
+        ResultBG.SetActive(true);
+        SkillResultsText.GetComponent<Text>().text = "You Rolled: " + lastResult + "\nfor " + Skill_Handler.skillNames[skillNumber-1];
+
+    }
+
+    public void onclickHideResults()
+    {
+        GameObject.Find("Roll Result BG").SetActive(false);
     }
 
 }
