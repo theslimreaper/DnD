12e73bf8d70f8349e62d1f5b4322cd4455731865
unknown <dokeefe@MP05VH80.campus.ltu.edu>
2015-10-04 11:29:14 -0400

    no message

---
+++ DnD/Assets/Scripts/Known_Spells.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Known_Spells : MonoBehaviour
+{
+	public static string[] spellLevel = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
