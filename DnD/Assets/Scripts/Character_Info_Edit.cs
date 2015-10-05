a9ec41176b7777528fe36437b9f874356f10c7e0
unknown <dokeefe@MP05VH80.campus.ltu.edu>
2015-10-05 17:24:42 -0400

    edit, close. edit, close. Can now switch between edit and display mode on character info screen

---
+++ DnD/Assets/Scripts/Character_Info_Edit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Character_Info_Edit : MonoBehaviour {
+
+	public GameObject editscreen;
+	public GameObject displayscreen;
+
+	void Start(){
+		DisplayMode ();
+	}
+
+	public void EditMode(){
+		editscreen.SetActive (true);
+		displayscreen.SetActive (false);
+	}
+
+	public void DisplayMode(){
+		editscreen.SetActive (false);
+		displayscreen.SetActive (true);
+	}
+}
