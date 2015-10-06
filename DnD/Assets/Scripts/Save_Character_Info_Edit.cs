4b44334f9fde4ab7168cbb546357f00af7ebc0b1
unknown <dokeefe@MP05VH80.campus.ltu.edu>
2015-10-06 15:06:39 -0400

    First part of script for saving edit data to the display form.

---
+++ DnD/Assets/Scripts/Save_Character_Info_Edit.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Save_Character_Info_Edit : MonoBehaviour {
+
+	public GameObject editName;
+	public GameObject displayName;
+	public GameObject editClass;
+	public GameObject displayClass;
+	public GameObject editRace;
+	public GameObject displayRace;
+	public GameObject editAlignment;
+	public GameObject displayAlignment;
+	public GameObject editAge;
+	public GameObject displayAge;
+	public GameObject editGender;
+	public GameObject displayGender;
+	public GameObject editLevel;
+	public GameObject displayLevel;
+	public GameObject editHP;
+	public GameObject displayHP;
+	public GameObject editHeight;
+	public GameObject displayHeight;
+	public GameObject editWeight;
+	public GameObject displayWeight;
+	public GameObject editCarry;
+	public GameObject displayCarry;
+	public GameObject editSpeed;
+	public GameObject displaySpeed;
+	public GameObject editLanguage;
+	public GameObject displayLanguage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void Edit(){
+		displayName = editName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
