458111407c73e93e2d624c66e756c4758a004759
zonte94 <mborsos94@gmail.com>
2015-10-12 21:47:08 -0400

    Finished dynamic buttons for loading characters in main and side menu. Still needs a scrollview implemented for it.

--- DnD/Assets/Scripts/Load_Character_Selection.cs
+++ DnD/Assets/Scripts/Load_Character_Selection.cs
@@ -8,6 +8,7 @@ public class Load_Character_Selection : MonoBehaviour {
     public GameObject Select_Character_Button;
     public GameObject Select_Character_Text;
     public string[] characters = null;
+    public List<GameObject> dynamicObjects = new List<GameObject>();
     public RectTransform ParentButton;
     public RectTransform ParentText;
 
@@ -18,32 +19,38 @@ public class Load_Character_Selection : MonoBehaviour {
     public void GetCharacters()
     {
         characters  = Directory.GetFiles(".","*.xml");
-        for(int i = 0; i < characters.Length; i++)
+        foreach( var item in dynamicObjects )
         {
-            GameObject characterButton = (GameObject)Instantiate(Select_Character_Button);
-            GameObject characterText = characterButton.gameObject.transform.GetChild(0).gameObject;
-            characterButton.transform.SetParent(ParentButton, false);
-            //characterText.transform.SetParent(ParentText, false);
-            characterButton.transform.localScale = new Vector3(1, 1, 1);
-            characterText.transform.localScale = new Vector3(1, 1, 1);
-            characterText.GetComponent<Text>().text = Path.GetFileNameWithoutExtension(characters[i]);
-            if( i == 0)
-            {
-                characterButton.transform.position = new Vector3(ParentButton.transform.position.x, ParentButton.transform.position.y, -212);
-                characterText.transform.position = new Vector3(ParentText.transform.position.x, ParentText.transform.position.y + 150, ParentText.transform.position.z);
-            }
-            else
-            {
-                characterButton.transform.position = new Vector3(ParentButton.transform.position.x, ParentButton.transform.position.y - (150*i), -212);
-                characterText.transform.position = new Vector3(ParentText.transform.position.x, ParentText.transform.position.y + (150*i), ParentText.transform.position.z);
-            }
- 
-            print(characterText.GetComponent<Text>().text);
-            Button tempButton = characterButton.GetComponent<Button>();
-            int position = i;
-
-            tempButton.onClick.AddListener(() => SelectCharacter(position));
+            Destroy(item);
         }
+
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                GameObject characterButton = (GameObject)Instantiate(Select_Character_Button);
+                GameObject characterText = characterButton.gameObject.transform.GetChild(0).gameObject;
+                characterButton.transform.SetParent(ParentButton, false);
+                //characterText.transform.SetParent(ParentText, false);
+                characterButton.transform.localScale = new Vector3(0.4760417f, 0.4760417f, 0.4760417f);
+                characterText.transform.localScale = new Vector3(4.760417f * 0.5f, 4.760417f * 0.5f, 4.760417f * 0.5f);
+                characterText.GetComponent<Text>().text = Path.GetFileNameWithoutExtension(characters[i]);
+                if (i == 0)
+                {
+                    characterButton.transform.position = new Vector3(ParentText.transform.position.x, ParentText.transform.position.y, -212);
+                    //characterText.transform.position = new Vector3(ParentText.transform.position.x, ParentText.transform.position.y + 50, ParentText.transform.position.z);
+                }
+                else
+                {
+                    characterButton.transform.position = new Vector3(ParentText.transform.position.x, ParentText.transform.position.y - (150 * i), -212);
+                    // characterText.transform.position = new Vector3(ParentText.transform.position.x, ParentText.transform.position.y + (50*i), ParentText.transform.position.z);
+                }
+
+                dynamicObjects.Add( characterButton );
+                Button tempButton = characterButton.gameObject.GetComponent<Button>();
+                int position = i;
+
+                tempButton.onClick.AddListener(() => SelectCharacter(position));
+            }
 
     }
 
