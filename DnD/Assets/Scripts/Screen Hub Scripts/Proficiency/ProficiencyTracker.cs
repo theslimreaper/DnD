a5555368b6ec3cb8f12fea906878cfb6ba3d75ac
zonte94 <mborsos94@gmail.com>
2015-11-28 22:38:08 -0500

    Revert "Revert "no comment""
    This reverts commit d5e5056a19991fb15fcbf5693941e90d6fdd542c.

--- DnD/Assets/Scripts/Screen Hub Scripts/Proficiency/ProficiencyTracker.cs
+++ DnD/Assets/Scripts/Screen Hub Scripts/Proficiency/ProficiencyTracker.cs
@@ -4,13 +4,24 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ProficiencyTracker : MonoBehaviour {
-	public List<string> Proficiencies;
+	public static List<string> Proficiencies = new List<string>();
 	public GameObject contentPane;
 	public GameObject Popup;
 	public InputField newValue;
 	public GameObject ProficiencyPrefab;
 	public GameObject currentItem;
-
+	public Text BonusText;
+	float bonus;
+	public void start()
+	{
+		if(Character_Info.characterLevel==null||Character_Info.characterLevel=="")
+		{
+			Character_Info.characterLevel="1";
+		}
+		bonus =(1f + System.Convert.ToInt32(Character_Info.characterLevel))/4f;
+		bonus = Mathf.Ceil (bonus);
+		BonusText.text="Bonus: +"+bonus.ToString();
+	}
 	public void promptForValue()
 	{
 		Popup.SetActive (true);
@@ -20,6 +31,7 @@ public class ProficiencyTracker : MonoBehaviour {
 	{
 		if(newValue.text!=null&&newValue.text.Length!=0)
 		{
+			print (newValue.text);
 			Proficiencies.Add(newValue.text);
 			newValue.text="";
 		}
@@ -31,7 +43,7 @@ public class ProficiencyTracker : MonoBehaviour {
 		//remove all old values
 		foreach(Transform child in contentPane.transform) 
 		{
-			Destroy(child);
+			Destroy(child.gameObject);
 		}
 		//add all values in 
 		for(int i=0;i<Proficiencies.Count;i++)
@@ -39,8 +51,8 @@ public class ProficiencyTracker : MonoBehaviour {
 			currentItem=Instantiate (ProficiencyPrefab, new Vector3(0,0,0),Quaternion.identity) as GameObject;
 			currentItem.transform.parent=contentPane.transform;
 			currentItem.GetComponent<Text>().text=Proficiencies[i];
-			currentItem.transform.localPosition= new Vector3(0,160-i*20,0);
-			//currentItem.GetComponent<RectTransform>().localPosition=new Vector3(0,160-i*20,0);
+			currentItem.transform.position= contentPane.transform.position;
+			currentItem.transform.position=	new Vector3(currentItem.transform.position.x+91,currentItem.transform.position.y-27.5f-20f*i,currentItem.transform.position.z);
 		}
 	}
 }
