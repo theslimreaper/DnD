4f0e0f7e51486e95d7e53565c4c4f9c07a50f7b8
zonte94 <mborsos94@gmail.com>
2015-11-30 16:04:20 -0500

    Credited the 5th edition player's handbook and WotC in the credits scene

--- DnD/Assets/Scripts/Miscellaneous/Credits_Scroller.cs
+++ DnD/Assets/Scripts/Miscellaneous/Credits_Scroller.cs
@@ -12,7 +12,7 @@ public class Credits_Scroller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         credits.transform.position= new Vector3 (credits.transform.position.x,credits.transform.position.y+.5f*speed,credits.transform.position.z);
-        float position = credits.transform.position.y;
+        float position = ( credits.transform.position.y - ((700*Screen.height) / 1080));
         if(position > Screen.height)
         {
             Application.LoadLevel("Start Screen");
