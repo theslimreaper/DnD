77d33848468d66b1d8283d6d1483dc23df5a8c8e
Joel Vann <Jvann@ltu.edu>
2015-12-10 23:59:52 -0500

    minor tweaks to how I had the dynamic viewer working.

--- DnD/Assets/Scripts/DynamicScrollView.cs
+++ DnD/Assets/Scripts/DynamicScrollView.cs
@@ -2,19 +2,24 @@
 using System.Collections;
 
 public class DynamicScrollView : MonoBehaviour {
-	float lowestNumber=0;
-	float extraHeight = 0;
+	public float lowestNumber=0;
+	public float extraHeight = 0;
 	// Use this for initialization
 	public void Update()
 	{
+		int i = 0;
 		foreach(Transform child in transform)
 		{
+			if(i==0)
+				extraHeight=child.GetComponent<RectTransform>().rect.height;
 			if(child.transform.position.y<lowestNumber)
 			{
 				lowestNumber=child.transform.localPosition.y;
 				extraHeight=child.GetComponent<RectTransform>().rect.height;
 			}
+			i++;
 		}
+		i = 0;
 		//print ("The lowest number found was: " + lowestNumber + "\nThe Extra Height is: " + extraHeight);
 		this.GetComponent<RectTransform>().sizeDelta = new Vector2(0, extraHeight-lowestNumber);
 	}
